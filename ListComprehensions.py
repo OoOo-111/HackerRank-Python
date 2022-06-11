fca35af5a3dd4cb154a2916d5698f87b4d8eedb3
OoOo-111 <atasoyarda14@gmail.com>
2022-06-12 00:56:27 +0300

    first commit

---
+++ ListComprehensions.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    
+    lis =[]
+    
+    for x in range(x+1):
+        for y in range(y+1):
+            for z in range(z+1):
+              sum = x+y+z 
+              if sum != n:
+                  lis.append([x,y,z])
+    
+    
+    print(lis)
